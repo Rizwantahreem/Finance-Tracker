bfa7bbc61ed729b1811c94565495b805d596afc2
Tahreem Rizwan <tahreem.1701203@gmail.com>
2025-12-16 12:38:33 +0500

    transaction apis changes connected to mongo db changed all transaction apis from static data handling

---
+++ DbConnection.js
@@ -0,0 +1,40 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import mongoose from "mongoose";
+
+const client = new MongoClient(
+    process.env.CONNECTION_STRING, 
+    {
+        serverSelectionTimeoutMS: 30000, // Set timeout to 30 seconds
+        socketTimeoutMS: 45000,          // Set socket timeout to 45 seconds
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationError: true,
+        }
+    }
+);
+
+export const startServer = async (app, port, req, res, next) => {
+    try {
+        await mongoose.connect(
+            process.env.CONNECTION_STRING, 
+            {
+                serverSelectionTimeoutMS: 30000, // Set timeout to 30 seconds
+                socketTimeoutMS: 45000,          // Set socket timeout to 45 seconds
+                serverApi: {
+                    version: ServerApiVersion.v1,
+                    strict: true,
+                    deprecationError: true,
+                }
+            }
+        )
+
+        console.log('Connected to MongoDB');
+
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`)
+        });
+    } catch (error) {
+        next({msg: error.msg, status: 500});
+    }
+}
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import express, { urlencoded } from 'express';
 import router from './routers/transaction.js';
+import { errorLogger } from './middlwares/error.middleware.js';
+import { startServer } from './DbConnection.js';
 
 const app = express();
 const port = process.env.Port || 5000;
+
+startServer(app, port);
 
 // Adding middleware for request body parsing 
 app.use(express.json());
 app.use(urlencoded({ extended: false }));
 
-app.use('/api/transaction', router);
+app.use('/api/transaction', router, errorLogger);
+app.use(errorLogger);
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})

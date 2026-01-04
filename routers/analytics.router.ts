27d433f2d5d8902b91b54a2dc15aabdc32da2d15
Tahreem Rizwan <tahreem.1701203@gmail.com>
2026-01-05 00:24:04 +0500

    added some analytics apis

--- routers/analytics.router.ts
+++ routers/analytics.router.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import {
   getexpenseToBudgetComparison,
-  getMonthlyExpenses,
+  getDetailedMonthlyExpenses,
+  getRecentTransactions,
   getSummary,
 } from "../controllers/analytics.controller.js";
 
 const analyticRouter = Router();
 
 analyticRouter.get("/summary", getSummary);
-analyticRouter.get("/month-in-glance", getMonthlyExpenses);
+analyticRouter.get("/month-in-glance", getDetailedMonthlyExpenses);
 analyticRouter.get("/budget tracking", getexpenseToBudgetComparison);
+analyticRouter.get("/recent-transactions", getRecentTransactions);
 
 export default analyticRouter;
--- server.ts
+++ server.ts
@@ -8,6 +8,7 @@ import { verifyToken } from "./middlewares/auth.middleware.js";
 import { startServer } from "./config/DbConnection.js";
 import transactionRouter from "./routers/transaction.router.js";
 import budgetRouter from "./routers/budget.router.js";
+import analyticRouter from "./routers/analytics.router.js";
 
 const app = express();
 const port = process.env.Port || 5000;
@@ -23,5 +24,6 @@ app.use("/api/transaction", verifyToken, transactionRouter);
 app.use("/api/user", userRouter);
 app.use("/api/category", verifyToken, categoryRouter);
 app.use("/api/budget", verifyToken, budgetRouter);
+app.use("/api/dashboard", verifyToken, analyticRouter);
 
 app.use(errorLogger);
